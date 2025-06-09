318c92db08cabc571a108aa0e52105e6547a6893
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-06-09 13:12:15 +0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -6,9 +6,11 @@ $username = "if0_39047715";
 $password = "Kimdung16091961";
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Kết nối thất bại: " . $e->getMessage());
 }
 ?>
+
+
