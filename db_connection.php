1d1b05229bcdd566b5b2c95d99148c6fabd89046
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-06-12 11:48:16 +0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -6,13 +6,9 @@ $username = "if0_39047715";
 $password = "Kimdung16091961";
 
 try {
-    // Thiết lập kết nối PDO
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-
-    // Thiết lập chế độ lỗi để phát hiện lỗi rõ ràng
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    // Thông báo lỗi rõ ràng và dừng thực thi
-    die("Lỗi kết nối server! " . $e->getMessage());
+    die("Kết nối thất bại: " . $e->getMessage());
 }
 ?>
