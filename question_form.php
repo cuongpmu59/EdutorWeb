e0cef0b4fea5ece4ff63ced0ccfe6fbd52c63a3f
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-06-15 17:42:28 +0700

    Update question_form.php

--- question_form.php
+++ question_form.php
@@ -106,14 +106,5 @@
   });
 </script>
 
-<script>
-  // Khi toàn bộ nội dung HTML đã tải xong
-  window.addEventListener("DOMContentLoaded", function () {
-    if (window.MathJax) {
-      MathJax.typesetPromise();
-    }
-  });
-</script>
-
 </body>
 </html>
