a1c1e6b50851fb8c6ac9dede6bc21311de01bb66
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-06-15 16:09:36 +0700

    Update examdisplay.php

--- examdisplay.php
+++ examdisplay.php
@@ -6,9 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Thi Trắc Nghiệm</title>
   <link rel="stylesheet" href="css/styles_Exam.css">
-  <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-  <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
-
+  
   <!-- CẤU HÌNH MATHJAX -->
   <script>
     window.MathJax = {
