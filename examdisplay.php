313681b0b0996f3c403abf36253443a8d52706fb
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-06-22 18:08:07 +0700

    Update examdisplay.php

--- examdisplay.php
+++ examdisplay.php
@@ -28,9 +28,9 @@
 
   <!-- TẢI THƯ VIỆN MATHJAX -->
   <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-<script id="MathJax-script" async
-  src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
-</script>
+    <script id="MathJax-script" async
+    src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
+  </script>
 </head>
 
 <body>
