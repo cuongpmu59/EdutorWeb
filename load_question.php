80813e68fdb79eaf88e6f6a088432a9fd600acb1
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-06-22 19:28:51 +0700

    Update load_question.php

--- load_question.php
+++ load_question.php
@@ -25,7 +25,7 @@ function latexWrap($str) {
 
 
 // Lấy chủ đề từ GET hoặc gán mặc định
-$selectedTopic = $_GET['topic'] ?? 'Tích phân';
+$selectedTopic = $_GET['topic'] ?? 'TênChủĐềMặcĐịnh';
 
 try {
     $stmt = $conn->prepare("SELECT * FROM questions WHERE topic = :topic ORDER BY RAND() LIMIT 20");
