78b3e91ec1fa3918dddddd824389218ccdfdf449
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-06-27 15:03:27 +0700

    Update export_questions.php

--- export_questions.php
+++ export_questions.php
@@ -4,15 +4,20 @@ require 'db_connection.php';
 header('Content-Type: text/csv; charset=utf-8');
 header('Content-Disposition: attachment; filename=cau_hoi_export.csv');
 
+// BOM cho UTF-8 (Excel nhận dạng tiếng Việt đúng)
+echo "\xEF\xBB\xBF";
+
 $output = fopen('php://output', 'w');
 
-// Ghi dòng tiêu đề
-fputcsv($output, ['Câu hỏi', 'Đáp án A', 'Đáp án B', 'Đáp án C', 'Đáp án D', 'Đáp án đúng']);
+// Ghi tiêu đề
+fputcsv($output, ['ID', 'Chủ đề', 'Câu hỏi', 'Đáp án A', 'Đáp án B', 'Đáp án C', 'Đáp án D', 'Đáp án đúng']);
 
-$result = mysqli_query($conn, "SELECT question, answer1, answer2, answer3, answer4, correct_answer FROM questions");
+$result = mysqli_query($conn, "SELECT id, topic, question, answer1, answer2, answer3, answer4, correct_answer FROM questions");
 
 while ($row = mysqli_fetch_assoc($result)) {
     fputcsv($output, [
+        $row['id'],
+        $row['topic'],
         $row['question'],
         $row['answer1'],
         $row['answer2'],
