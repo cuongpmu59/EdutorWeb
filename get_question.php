11f9d571c8ddc86045a3607e24d4db6431bc1e69
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-01 15:01:23 +0700

    Update get_question.php

--- get_question.php
+++ get_question.php
@@ -33,8 +33,14 @@ try {
 
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <!-- DataTables Buttons -->
+
+    <!-- DataTables Core -->
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+
+    <!-- JSZip (bắt buộc cho Excel export) -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+
+    <!-- Buttons Extension -->
     <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
