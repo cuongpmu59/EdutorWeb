2fccee3e572cefd1fcd3165110ac12cf0348d922
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-08 17:36:44 +0700

    Create mc_preview.php

---
+++ pages/mc/mc_preview.php
@@ -0,0 +1,8 @@
+// =======================
+// File: pages/mc/mc_preview.php
+// =======================
+?>
+<div id="mc_preview_area"><em>⚡ Xem trước câu hỏi sẽ hiển thị ở đây...</em></div>
+<img id="mc_preview_image" style="display:none; max-height:150px; margin-top:10px; border:1px solid #ccc;">
+<?php
+
