89e95ceffc3c4210745fb13460f31f1ba97899e9
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-08 17:37:13 +0700

    Create sa_preview.php

---
+++ pages/sa/sa_preview.php
@@ -0,0 +1,7 @@
+// =======================
+// File: pages/sa/sa_preview.php
+// =======================
+?>
+<div id="sa_preview_area"><em>⚡ Xem trước câu hỏi tự luận sẽ hiển thị ở đây...</em></div>
+<img id="sa_preview_image" style="display:none; max-height:150px; margin-top:10px; border:1px solid #ccc;">
+<?php
