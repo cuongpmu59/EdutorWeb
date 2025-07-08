32b847021368756b253c93a70ce20e3cbb86de63
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-08 17:36:54 +0700

    Delete mc_preview.php

--- pages/mc_preview.php
+++
@@ -1,6 +0,0 @@
-<!-- pages/mc_preview.php -->
-<div class="preview-box">
-  <h3>🔍 Xem trước câu hỏi trắc nghiệm</h3>
-  <button type="button" id="btn_preview">🔁 Cập nhật xem trước</button>
-  <div id="mc_preview_content" class="preview-content mt-2"></div>
-</div>
