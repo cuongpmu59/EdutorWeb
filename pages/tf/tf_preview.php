4d1e0300015070cb6a6d67c4f07a62e7255e30af
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-08 17:37:37 +0700

    Create tf_preview.php

---
+++ pages/tf/tf_preview.php
@@ -0,0 +1,8 @@
+// =======================
+// File: pages/tf/tf_preview.php
+// =======================
+?>
+<div id="tf_preview_area"><em>⚡ Xem trước câu hỏi Đúng/Sai sẽ hiển thị ở đây...</em></div>
+<img id="tf_preview_image" style="display:none; max-height:150px; margin-top:10px; border:1px solid #ccc;">
+<?php
+
