7aa0a6821ce03cb71feb23f28a9f4a148bcdfba7
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-08 17:37:26 +0700

    Delete tf_preview.php

--- pages/tf_preview.php
+++
@@ -1,6 +0,0 @@
-<!-- pages/tf_preview.php -->
-<div class="preview-box">
-  <h3>🔍 Xem trước câu hỏi Đúng/Sai</h3>
-  <button type="button" id="btn_preview">🔁 Cập nhật xem trước</button>
-  <div id="tf_preview_content" class="preview-content mt-2"></div>
-</div>
