515706a5e08fb4d9950cb9f62f7e09f76f5a88e7
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-08 17:37:31 +0700

    Create tf_table.php

---
+++ pages/tf/tf_table.php
@@ -0,0 +1,7 @@
+// =======================
+// File: pages/tf/tf_table.php
+// =======================
+?>
+<iframe src="get_question.php?type=tf" width="100%" height="500" style="border:1px solid #aaa;"></iframe>
+<?php
+
