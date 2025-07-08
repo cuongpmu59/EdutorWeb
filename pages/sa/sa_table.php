cbb7f365e4d574ba11a477ba5919d9f86db5a191
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-08 17:37:17 +0700

    Create sa_table.php

---
+++ pages/sa/sa_table.php
@@ -0,0 +1,6 @@
+// =======================
+// File: pages/sa/sa_table.php
+// =======================
+?>
+<iframe src="get_question.php?type=sa" width="100%" height="500" style="border:1px solid #aaa;"></iframe>
+<?php
