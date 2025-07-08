1d69ecf81e60316404d3873669e7da626d9e2138
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-08 17:36:49 +0700

    Create mc_table.php

---
+++ pages/mc/mc_table.php
@@ -0,0 +1,6 @@
+// =======================
+// File: pages/mc/mc_table.php
+// =======================
+?>
+<iframe src="get_question.php?type=mc" width="100%" height="500" style="border:1px solid #aaa;"></iframe>
+<?php
