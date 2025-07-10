e4de90c84604ff20585e63b895c01085bbe92712
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-10 15:40:40 +0700

    Update tf_form.php

--- pages/tf/tf_form.php
+++ pages/tf/tf_form.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <title>✔️ Câu hỏi Đúng/Sai</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/main_ui.css">
+  <link rel="stylesheet" href="../../css/main_ui.css">
   <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
   <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 </head>
