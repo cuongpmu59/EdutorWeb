bd1e140d5316123c98e5e4a80832dd1470307f6b
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-15 05:31:34 +0700

    Update filter.php

--- utils/filter.php
+++ utils/filter.php
@@ -1,21 +1,3 @@
-<?php
-require_once __DIR__ . '/../db_connection.php';
-
-$topics = [];
-try {
-  $stmtTopics = $conn->query("SELECT DISTINCT mc_topic FROM mc_questions WHERE mc_topic IS NOT NULL AND mc_topic != '' ORDER BY mc_topic");
-  $topics = $stmtTopics->fetchAll(PDO::FETCH_COLUMN);
-} catch (Exception $e) {
-  $topics = [];
-}
-
-// Xuất HTML các option để nhúng vào select
-foreach ($topics as $tp) {
-  echo "<option value='" . htmlspecialchars($tp) . "'>" . htmlspecialchars($tp) . "</option>";
-}
-?>
-
-<!-- utils/filter.php -->
 <div class="filter-left">
   📚 Chủ đề:
   <select id="filter-topic">
