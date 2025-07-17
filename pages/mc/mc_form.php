ef3f55c0c13f0c8fd93826f7aa19f2a914d9e077
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-17 15:14:38 +0700

    Update mc_form.php

--- pages/mc/mc_form.php
+++ pages/mc/mc_form.php
@@ -5,6 +5,8 @@
   <meta charset="UTF-8">
   <title>Nhập câu hỏi trắc nghiệm nhiều lựa chọn</title>
   <link rel="stylesheet" href="/css/form.css">
+  <link rel="stylesheet" href="/css/main_ui.css">
+
   <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
   <script src="/js/mc_form.js" defer></script>
