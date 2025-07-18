21a1ff8edf32e19cf6cd17af57b226f21fc9cf5c
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-18 17:56:46 +0700

    Delete question_modal.php

--- includes/question_modal.php
+++
@@ -1,5 +0,0 @@
-<!-- Modal hiển thị ảnh -->
-<div id="imageModal" style="display:none; position:fixed; z-index:9999; top:0; left:0; width:100%; height:100%; background:rgba(0,0,0,0.85); justify-content:center; align-items:center;">
-  <span onclick="closeModal()" style="position:absolute; top:10px; right:20px; color:white; font-size:28px; cursor:pointer;">&times;</span>
-  <img id="modalImage" style="max-width:90%; max-height:90%;">
-</div>
