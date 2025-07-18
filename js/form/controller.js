b20cad190a94f29e90b8fedbc18c20f3e844c829
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-18 07:36:25 +0700

    Create controller.js

---
+++ js/form/controller.js
@@ -0,0 +1,5 @@
+// Gom các module con và export lại theo namespace
+export * as formView from './formView.js';
+export * as imageManager from './imageManager.js';
+export * as previewView from './previewView.js';
+export * as tableView from './tableView.js';
