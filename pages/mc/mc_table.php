3ed5ea3cabb3cbded533995a8c8e7cb421391d42
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-18 14:55:04 +0700

    Update mc_table.php

--- pages/mc/mc_table.php
+++ pages/mc/mc_table.php
@@ -20,7 +20,7 @@ try {
   <title>📋 Câu hỏi Nhiều lựa chọn</title>
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
   <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
-  <link rel="stylesheet" href="../../css/main_ui.css">
+  <link rel="stylesheet" href="../../css/table/table.css">
   
   <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
   <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
