57661301a4b7fc70480a97a8a25e23482fc5b95e
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-19 06:07:26 +0700

    Update mc_form.php

--- pages/mc/mc_form.php
+++ pages/mc/mc_form.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Nhập câu hỏi trắc nghiệm</title>
-  <link rel="stylesheet" href="/css/mc_form.css">
+  <link rel="stylesheet" href="/css/main_ui.css">
 </head>
 <body>
   <div class="form-container">
