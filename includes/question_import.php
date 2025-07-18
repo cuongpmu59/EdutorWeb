f6383d93cb4b8c80d177ebee392e00a50627546f
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-18 17:56:51 +0700

    Delete question_import.php

--- includes/question_import.php
+++
@@ -1,7 +0,0 @@
-<div id="importTab" class="tab-content">
-  <label><strong>📤 Nhập từ Excel:</strong></label>
-  <input type="file" id="excelInput" accept=".xlsx,.xls">
-  <br><br>
-  <button onclick="$('.buttons-excel').click()">📥 Xuất Excel</button>
-  <button onclick="$('.buttons-print').click()">🖨️ In bảng</button>
-</div>
