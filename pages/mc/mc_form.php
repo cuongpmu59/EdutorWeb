b1b1f977f01642401efcc8cfc14404a23f1db200
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-19 05:56:52 +0700

    Update mc_form.php

--- pages/mc/mc_form.php
+++ pages/mc/mc_form.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <title>Nhập câu hỏi trắc nghiệm</title>
-  <link rel="stylesheet" href="/css/main_ui.css">
+  <link rel="stylesheet" href="/css/mc/mc_form.css">
   <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 </head>
 <body>
