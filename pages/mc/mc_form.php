bbc1fd64b763acc449718b25470b4bbdbae3886c
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-20 11:01:57 +0700

    Update mc_form.php

--- pages/mc/mc_form.php
+++ pages/mc/mc_form.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 session_start();
 
 // Kết nối CSDL qua hệ thống dotenv
-require_once __DIR__ . '/includes/db_connection.php';
+require_once __DIR__ . '/../../includes/db_connection.php';
 
 $mc = null;
 if (!empty($_GET['mc_id'])) {
