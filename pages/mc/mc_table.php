d6bd103d13bc5c5b94c54f5784e179299c02b747
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-20 20:35:41 +0700

    Update mc_table.php

--- pages/mc/mc_table.php
+++ pages/mc/mc_table.php
@@ -59,9 +59,19 @@ try {
 </div>
 
 <!-- Modal ảnh -->
-<div id="imgModal" style="display:none; position:fixed;top:0;left:0;width:100%;height:100%;background:#000000bb;align-items:center;justify-content:center;z-index:1000;">
-  <img id="imgModalContent" src="" style="max-width:90%;max-height:90%;border:4px solid white;box-shadow:0 0 10px white;">
+<div id="imgModal" style="
+  display: none;
+  position: fixed;
+  top: 0; left: 0;
+  width: 100%; height: 100%;
+  background: #000000bb;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+  display: flex;">
+  <img id="imgModalContent" src="" style="max-width:90%; max-height:90%; border:4px solid white; box-shadow:0 0 10px white;">
 </div>
+
 
 <input type="file" id="excelFile" accept=".xlsx" />
 
