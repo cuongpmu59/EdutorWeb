860ba9fe4366468702d9fc79646c1c06b50cafce
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-21 10:55:37 +0700

    Update mc_filter.php

--- includes/mc_filter.php
+++ includes/mc_filter.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../db_connection.php';
+require_once __DIR__ . '/../../includes/db_connection.php';
 $stmt = $conn->query("SELECT DISTINCT mc_topic FROM mc_questions WHERE mc_topic IS NOT NULL AND mc_topic != '' ORDER BY mc_topic");
 ?>
 <select id="filter-topic">
