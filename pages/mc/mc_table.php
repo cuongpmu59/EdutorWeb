3a9bc94fe3207f0644554f73772e81c9680c544a
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-22 10:50:51 +0700

    Update mc_table.php

--- pages/mc/mc_table.php
+++ pages/mc/mc_table.php
@@ -86,7 +86,12 @@ try {
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
 
 <!-- Script chính -->
-<script src="../../js/table/mc_table.js"></script>
+<script src="../../js/table/mc_table_function.js"></script>
+<script src="../../js/table/mc_table_image.js"></script>
+<script src="../../js/table/mc_table_transmittion.js"></script>
+<script src="../../js/table/mc_table_excel.js"></script>
+<script src="../../js/table/mc_table_arrow.js"></script>
+
 
 </body>
 </html>
