3881bf6e81fa57969dbd2c5057651a6a0d0bde1d
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-24 06:52:42 +0700

    Update mc_layout.js

--- js/form/mc_layout.js
+++ js/form/mc_layout.js
@@ -1,8 +1,6 @@
 document.getElementById('mc_view_list').addEventListener('click', () => {
   const wrapper = document.getElementById('mcTableWrapper');
-  if (wrapper.style.display === 'none' || wrapper.style.display === '') {
-    wrapper.style.display = 'block';
-  } else {
-    wrapper.style.display = 'none';
-  }
+  wrapper.style.display = (wrapper.style.display === 'none' || !wrapper.style.display)
+    ? 'block'
+    : 'none';
 });
