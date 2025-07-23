8f9f9174c8a283985ecbc9a92d3784cd60f001e8
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-23 15:35:54 +0700

    Update mc_table_function.js

--- js/table/mc_table_function.js
+++ js/table/mc_table_function.js
@@ -22,12 +22,34 @@ $(document).ready(function () {
       }
     ],
     createdRow: function (row, data) {
-      const fields = ['mc_id', 'mc_topic', 'mc_question', 'mc_answer1', 'mc_answer2', 'mc_answer3', 'mc_answer4', 'mc_correct_answer', 'mc_image_url'];
+      const fields = [
+        'mc_id', 'mc_topic', 'mc_question',
+        'mc_answer1', 'mc_answer2', 'mc_answer3', 'mc_answer4',
+        'mc_correct_answer', 'mc_image_url'
+      ];
       $('td', row).each(function (index) {
         $(this).attr('data-raw', data[fields[index]]);
       });
     },
     responsive: true,
-    scrollX: true
+    scrollX: true,
+    initComplete: function (settings, json) {
+      const topics = [...new Set(json.map(q => q.mc_topic).filter(Boolean))].sort();
+      const $filter = $('#topicFilter');
+      if ($filter.length > 0) {
+        topics.forEach(topic => {
+          $filter.append(`<option value="${topic}">${topic}</option>`);
+        });
+
+        $filter.on('change', function () {
+          const selected = $(this).val();
+          if (selected) {
+            table.column(1).search('^' + selected + '$', true, false).draw();
+          } else {
+            table.column(1).search('').draw();
+          }
+        });
+      }
+    }
   });
 });
