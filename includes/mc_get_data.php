b8fd38621d7b662d938e05437f2fe4c8ba95ca96
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-24 10:19:03 +0700

    Update mc_get_data.php

--- includes/mc_get_data.php
+++ includes/mc_get_data.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../db_connection.php';
+require_once __DIR__ . '/../../includes/db_connection.php';
 
 header('Content-Type: application/json; charset=utf-8');
 error_reporting(E_ALL);
