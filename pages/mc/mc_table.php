c98f380e4cc5d902e68ff3477bde781bf8be92b9
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-26 19:37:22 +0700

    Update mc_table.php

--- pages/mc/mc_table.php
+++ pages/mc/mc_table.php
@@ -87,9 +87,9 @@ try {
 <!-- Script chính -->
 <script src="../../js/table/mc_table_function.js"></script>
 <script src="../../js/table/mc_table_image.js"></script>
-<script src="../../js/table/mc_table_transmittion.js"></script>
 <script src="../../js/table/mc_table_excel.js"></script>
 <script src="../../js/table/mc_table_arrow.js"></script>
+<script src="../../js/table/mc_table_transmittion.js"></script>
 
 </body>
 </html>
