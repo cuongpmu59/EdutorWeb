79fbca9385d3b89143c45ae2727f1b397194247f
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-26 15:18:50 +0700

    Update mc_table_transmittion.js

--- js/table/mc_table_transmittion.js
+++ js/table/mc_table_transmittion.js
@@ -1,12 +1,19 @@
-$(document).ready(function () {
-  $('#mcTable tbody').on('click', 'tr', function () {
-    const mc_id = $(this).children('td').eq(0).data('raw');
-    
-    if (mc_id) {
-      window.parent.postMessage({
-        type: 'mc_select_row',
-        data: { mc_id }
-      }, '*');
-    }
-  });
+function sendRowData(row) {
+  const $cells = $(row.node()).children('td');
+  const mc_id = $cells.eq(0).data('raw');
+
+  if (mc_id) {
+    window.parent.postMessage({
+      type: 'mc_select_row',
+      data: { mc_id: mc_id }
+    }, '*');
+  }
+}
+
+$('#mcTable tbody').on('click', 'tr', function () {
+  $('#mcTable tbody tr').removeClass('selected');
+  $(this).addClass('selected');
+
+  const row = mcTable.row(this);
+  sendRowData(row);
 });
