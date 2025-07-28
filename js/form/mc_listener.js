402443fe9bcd2b8f2b41c338ce550b8729708061
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-28 10:17:40 +0700

    Update mc_listener.js

--- js/form/mc_listener.js
+++ js/form/mc_listener.js
@@ -1,19 +1,23 @@
 window.addEventListener('message', function (event) {
-    const data = event.data;
-    if (data.type === 'mc_select_row') {
-      $('#mc_topic').val(data.mc_topic);
-      $('#mc_question').val(data.mc_question);
-      $('#mc_answer1').val(data.mc_answer1);
-      $('#mc_answer2').val(data.mc_answer2);
-      $('#mc_answer3').val(data.mc_answer3);
-      $('#mc_answer4').val(data.mc_answer4);
-      $('#mc_correct_answer').val(data.mc_correct_answer);
-  
-      if (data.mc_image_url) {
-        $('#mc_preview_image').attr('src', data.mc_image_url).show();
-      } else {
-        $('#mc_preview_image').attr('src', '').hide();
-      }
+  const data = event.data;
+  if (data.type === 'mc_select_row') {
+    $('#mc_topic').val(data.mc_topic);
+    $('#mc_question').val(data.mc_question);
+    $('#mc_answer1').val(data.mc_answer1);
+    $('#mc_answer2').val(data.mc_answer2);
+    $('#mc_answer3').val(data.mc_answer3);
+    $('#mc_answer4').val(data.mc_answer4);
+    $('#mc_correct_answer').val(data.mc_correct_answer);
+
+    if (data.mc_image_url) {
+      $('#mc_preview_image').attr('src', data.mc_image_url).show();
+    } else {
+      $('#mc_preview_image').attr('src', '').hide();
     }
-  });
-  
+
+    // Set hidden field mc_id (nếu cần)
+    $('#mc_id').length
+      ? $('#mc_id').val(data.mc_id)
+      : $('#mcForm').append(`<input type="hidden" id="mc_id" name="mc_id" value="${data.mc_id}">`);
+  }
+});
