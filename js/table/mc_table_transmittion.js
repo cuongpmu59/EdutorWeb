d275bf7cbe7ab86cb6867d1372c278b1eaeeae61
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-28 12:09:06 +0700

    Update mc_table_transmittion.js

--- js/table/mc_table_transmittion.js
+++ js/table/mc_table_transmittion.js
@@ -1,25 +1,22 @@
+// File: js/table/mc_table_transmittion.js
+
 $(document).ready(function () {
-  const table = $('#mc_table').DataTable();
+  const table = $('#mcTable').DataTable();
 
-  $('#mc_table tbody').on('click', 'tr', function () {
-    const row = table.row(this).node();
-    const data = table.row(this).data();
-    if (!data) return;
-
-    // Lấy URL từ ảnh trong cột 8 (dùng jQuery tìm thẻ <img> trong cột 8 của dòng đó)
-    const imgSrc = $('td:eq(8) img', row).attr('src') || '';
+  $('#mcTable tbody').on('click', 'tr', function () {
+    const $tds = $(this).find('td');
 
     const message = {
       type: 'mc_select_row',
-      mc_id: data[0],
-      mc_topic: data[1],
-      mc_question: data[2],
-      mc_answer1: data[3],
-      mc_answer2: data[4],
-      mc_answer3: data[5],
-      mc_answer4: data[6],
-      mc_correct_answer: data[7],
-      mc_image_url: imgSrc
+      mc_id: $tds.eq(0).data('raw'),
+      mc_topic: $tds.eq(1).data('raw'),
+      mc_question: $tds.eq(2).data('raw'),
+      mc_answer1: $tds.eq(3).data('raw'),
+      mc_answer2: $tds.eq(4).data('raw'),
+      mc_answer3: $tds.eq(5).data('raw'),
+      mc_answer4: $tds.eq(6).data('raw'),
+      mc_correct_answer: $tds.eq(7).data('raw'),
+      mc_image_url: $tds.eq(8).find('img').attr('src') || ''
     };
 
     window.parent.postMessage(message, window.location.origin);
