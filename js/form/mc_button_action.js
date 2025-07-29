3594b5a8fda1e50e27e971337d31459f38d4e4e1
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-29 08:28:32 +0700

    Update mc_button_action.js

--- js/form/mc_button_action.js
+++ js/form/mc_button_action.js
@@ -114,3 +114,37 @@ document.addEventListener("DOMContentLoaded", function () {
         else throw new Error("Upload thất bại");
     }
 });
+
+    // Xử lý nút làm lại
+
+document.getElementById('mc_reset').addEventListener('click', function () {
+    const form = document.getElementById('mcForm');
+  
+    form.querySelectorAll('input[type="text"], textarea').forEach(el => el.value = '');
+    form.querySelectorAll('select').forEach(sel => sel.selectedIndex = 0);
+    const img = document.getElementById('mc_preview_image');
+    if (img) {
+      img.src = '';
+      img.style.display = 'none';
+    }
+  
+    form.querySelector('#mc_image').value = '';
+    const hiddenImage = form.querySelector('input[name="existing_image"]');
+    if (hiddenImage) hiddenImage.remove();
+  
+    document.querySelectorAll('.preview-box').forEach(div => {
+      div.innerHTML = '';
+      div.style.display = 'none';
+    });
+  
+    document.getElementById('mcPreview').style.display = 'none';
+    document.getElementById('mcPreviewContent').innerHTML = '';
+  
+    if (window.MathJax && window.MathJax.typeset) {
+      MathJax.typeset();
+    }
+  
+    const idInput = document.getElementById('mc_id');
+    if (idInput) idInput.remove();
+  });
+  
