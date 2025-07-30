f6929649cbff41a431b8ea03896208886bc7f84b
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-07-30 07:31:32 +0700

    Update mc_get_table.js

--- js/table/mc_get_table.js
+++ js/table/mc_get_table.js
@@ -1,25 +1,39 @@
+// js/table/mc_get_table.js
 $(document).ready(function () {
+    initMcQuestionTable();
+  });
+  
+  function initMcQuestionTable() {
     $('#mcTable').DataTable({
       ajax: '../../includes/mc_get_data.php',
       columns: [
-        { data: 'mc_id' },
-        { data: 'mc_topic' },
-        { data: 'mc_question' },
-        { data: 'mc_answer1' },
-        { data: 'mc_answer2' },
-        { data: 'mc_answer3' },
-        { data: 'mc_answer4' },
-        { data: 'mc_correct_anwer' },
+        { data: 'mc_id', title: 'ID' },
+        { data: 'mc_topic', title: 'Chủ đề' },
+        { data: 'mc_question', title: 'Câu hỏi' },
+        { data: 'mc_answer1', title: 'Đáp án 1' },
+        { data: 'mc_answer2', title: 'Đáp án 2' },
+        { data: 'mc_answer3', title: 'Đáp án 3' },
+        { data: 'mc_answer4', title: 'Đáp án 4' },
+        { data: 'mc_correct_anwer', title: 'Đúng' },
         {
           data: 'mc_image_url',
-          render: function (data, type, row) {
+          title: 'Ảnh',
+          render: function (data) {
             if (!data) return '';
-            return `<img src="${data}" alt="ảnh" style="width:50px; height:auto;">`;
+            const thumbUrl = data.includes('/upload/')
+              ? data.replace('/upload/', '/upload/w_50,h_50,c_fill/')
+              : data;
+            return `<img src="${thumbUrl}" alt="Ảnh" width="50" height="50">`;
           },
           orderable: false,
           searchable: false
         }
-      ]
+      ],
+      language: {
+        url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/vi.json'
+      },
+      responsive: true,
+      pageLength: 10
     });
-  });
+  }
   
