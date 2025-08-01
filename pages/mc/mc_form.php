fed2b0d792a88a3ec96255af63877cb0ae529f7b
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-01 09:17:00 +0700

    Update mc_form.php

--- pages/mc/mc_form.php
+++ pages/mc/mc_form.php
@@ -126,7 +126,8 @@
   <script src="../../js/mc/mc_form_preview.js"></script>
   <script src="../../js/mc/mc_form_image.js"></script>
   <script src="../../js/mc/mc_form_button.js"></script>
-  <script src="../../js/form/mc_get_table.js"></script>
+  <!-- <script src="../../js/form/mc_fetch_data.js"></script> -->
+ 
   <script>
   // Lắng nghe dữ liệu từ iframe (bảng DataTable) gửi về
   window.addEventListener('message', function (event) {
