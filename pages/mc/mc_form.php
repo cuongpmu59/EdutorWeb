dee1f323be85719bc054a504878dcc2d027e96a0
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-02 08:31:59 +0700

    Update mc_form.php

--- pages/mc/mc_form.php
+++ pages/mc/mc_form.php
@@ -159,5 +159,12 @@
   });
 </script>
 
+<script>
+  document.getElementById('mc_view_list').addEventListener('click', function () {
+    const wrapper = document.getElementById('mcTableWrapper');
+    wrapper.style.display = (wrapper.style.display === 'none' || wrapper.style.display === '') ? 'block' : 'none';
+  });
+</script>
+
 </body>
 </html>
