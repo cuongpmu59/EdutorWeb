9d1f1f6a5158ba4ef5b4390328363dcf21a49232
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-02 09:40:52 +0700

    Update mc_form.php

--- pages/mc/mc_form.php
+++ pages/mc/mc_form.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <title>Câu hỏi trắc nghiệm</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <link rel="stylesheet" href="../../css/mc/mc_form_layout.css">
   <link rel="stylesheet" href="../../css/mc/mc_form_image.css">
   <link rel="stylesheet" href="../../css/mc/mc_form_preview.css">
