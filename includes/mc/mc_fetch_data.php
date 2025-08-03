2aa1320cad89f7e10b7ff5f435aec6067d1fefd8
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-04 06:44:36 +0700

    Update mc_fetch_data.php

--- includes/mc/mc_fetch_data.php
+++ includes/mc/mc_fetch_data.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/../includes/db_connection.php';
-//require_once __DIR__ . '/../env/config.php';
+require_once __DIR__ . '/../env/config.php';
 //require_once __DIR__ . '/../vendor/autoload.php';
 use Cloudinary\Uploader;
 
