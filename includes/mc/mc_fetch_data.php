066debe3da43b358b02ec695ed602942c3aefa96
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-04 06:32:28 +0700

    Update mc_fetch_data.php

--- includes/mc/mc_fetch_data.php
+++ includes/mc/mc_fetch_data.php
@@ -4,8 +4,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/../db_connection.php';
-require_once __DIR__ . '/../env/config.php';
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../env/config.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 use Cloudinary\Uploader;
 
 header('Content-Type: application/json');
