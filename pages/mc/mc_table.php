35ec0da000afbd087e884b8537c366d35e4f1e63
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-07 12:54:13 +0700

    Update mc_table.php

--- pages/mc/mc_table.php
+++ pages/mc/mc_table.php
@@ -1,10 +1,3 @@
-<?php
-require_once __DIR__ . '/../../includes/db_connection.php';
-if (!isset($conn)) {
-  die("❌ Không thể kết nối CSDL. Kiểm tra db_connection.php");
-}
-header("X-Frame-Options: SAMEORIGIN");
-?>
 <!DOCTYPE html>
 <html lang="vi">
 <head>
