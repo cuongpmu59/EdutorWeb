23fd2bbbb7dd273cf81e79a867cfe3479c20b349
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-08 09:48:48 +0700

    Update cloudinary_action.php

--- includes/mc/cloudinary_action.php
+++ includes/mc/cloudinary_action.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../env/config.php'; // chứa CLOUDINARY_* từ .env
+require_once __DIR__ . '/../../env/config.php'; // chứa CLOUDINARY_* từ .env
 
 header('Content-Type: application/json; charset=utf-8');
 
