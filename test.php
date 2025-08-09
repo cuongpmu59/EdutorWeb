475a975f7dd14c8ff9ad437b99bc5cde1b65af1f
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-09 08:16:12 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,4 @@
+<form action="upload_image.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="image" required />
+    <button type="submit">Upload Image</button>
+</form>
