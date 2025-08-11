b6aff2536d4fc14fb62042ba1ca831ce1bbcd559
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-11 12:04:47 +0700

    Delete test.php

--- test.php
+++
@@ -1,4 +0,0 @@
-<form action="upload_image.php" method="post" enctype="multipart/form-data">
-    <input type="file" name="image" required />
-    <button type="submit">Upload Image</button>
-</form>
