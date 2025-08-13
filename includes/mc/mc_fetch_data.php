b8da099c6723164c51b2c4141b9792e9e8f98cf2
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-13 15:55:10 +0700

    Update mc_fetch_data.php

--- includes/mc/mc_fetch_data.php
+++ includes/mc/mc_fetch_data.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/../../includes/db_connection.php';
 header('Content-Type: application/json; charset=utf-8');
 
