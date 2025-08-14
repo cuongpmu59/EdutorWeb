b7197e580d4220fedd1788142a84fb427f67fbcd
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-14 08:13:31 +0700

    Update mc_table.php

--- pages/mc/mc_table.php
+++ pages/mc/mc_table.php
@@ -3,18 +3,24 @@
 <head>
   <meta charset="UTF-8">
   <title>Quản lý câu hỏi</title>
+
+  <!-- MathJax -->
   <script>
-  window.MathJax = {
-    tex: {inlineMath: [['$', '$'], ['\\(', '\\)']]},
-    svg: {fontCache: 'global'}
-  };
+    window.MathJax = {
+      tex: { inlineMath: [['$', '$'], ['\\(', '\\)']] },
+      svg: { fontCache: 'global' }
+    };
   </script>
   <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js" async></script>
+
+  <!-- DataTables CSS -->
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
   <style>
     table img {
       border-radius: 4px;
       object-fit: cover;
+      max-width: 80px;
+      max-height: 80px;
     }
   </style>
 </head>
@@ -31,19 +37,67 @@
         <th>B</th>
         <th>C</th>
         <th>D</th>
-        <th>Đáp án</th>
-        <th>Hình minh hoạ</th>
+        <th>Đáp án</th>
+        <th>Hình minh họa</th>
       </tr>
     </thead>
   </table>
 
-  <!-- JS thư viện -->
+  <!-- jQuery + DataTables -->
   <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
   <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 
-  <!-- File JS khởi tạo bảng -->
-  <script src="../../js/mc/mc_fetch_data.js"></script>
-  <script src="../../js/mc/mc_table_arrow_key.js"></script>
+  <script>
+    $(function () {
+      $('#mcTable').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: {
+          url: '../../includes/mc/mc_fetch_data.php',
+          type: 'POST',
+          error: function (xhr) {
+            console.error('AJAX error:', xhr.status, xhr.responseText);
+            alert('Không thể tải dữ liệu (HTTP ' + xhr.status + ')');
+          }
+        },
+        order: [[0, 'desc']],
+        columns: [
+          { data: 'mc_id' },
+          { data: 'mc_topic' },
+          { data: 'mc_question' },
+          { data: 'mc_answer_a' },
+          { data: 'mc_answer_b' },
+          { data: 'mc_answer_c' },
+          { data: 'mc_answer_d' },
+          { data: 'mc_correct_answer' },
+          {
+            data: 'mc_image_url',
+            render: function (data) {
+              return data
+                ? '<img src="' + data + '" alt="ảnh" />'
+                : '';
+            }
+          }
+        ],
+        language: {
+          processing: 'Đang tải...',
+          search: 'Tìm:',
+          lengthMenu: 'Hiển thị _MENU_ dòng',
+          info: 'Hiển thị _START_–_END_ / _TOTAL_ dòng',
+          infoEmpty: 'Không có dữ liệu',
+          infoFiltered: '(lọc từ _MAX_ dòng)',
+          paginate: {
+            first: 'Đầu',
+            last: 'Cuối',
+            next: 'Sau',
+            previous: 'Trước'
+          },
+          zeroRecords: 'Không tìm thấy kết quả phù hợp'
+        }
+      });
+    });
+  </script>
 
+  <script src="../../js/mc/mc_table_arrow_key.js"></script>
 </body>
 </html>
