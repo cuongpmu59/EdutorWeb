403c7b96a68c6e7b8dcf7315f139700993a98273
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-14 08:55:53 +0700

    Update mc_fetch_data.php

--- includes/mc/mc_fetch_data.php
+++ includes/mc/mc_fetch_data.php
@@ -1,6 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header('Content-Type: application/json; charset=utf-8');
-require_once __DIR__ . '/../../includes/db_connection.php'; // file kết nối PDO
+require_once __DIR__ . '/../../includes/db_connection.php'; // phải tạo $pdo = new PDO(...)
 
 // Lấy params từ DataTables
 $draw   = isset($_POST['draw']) ? (int)$_POST['draw'] : 0;
