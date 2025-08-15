020c391a3342450b93c14331faea0e0420449298
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-15 10:19:23 +0700

    Update mc_form_button.js

--- js/mc/mc_form_button.js
+++ js/mc/mc_form_button.js
@@ -121,10 +121,10 @@
   }
 });
 
-  // Nút "Ẩn/hiện danh sách" (#mc_view_list)
-    document.getElementById('mc_view_list').addEventListener('click', () => {
-    const wrapper = document.getElementById('mcTableWrapper');
-    wrapper.style.display = (wrapper.style.display === 'none' || !wrapper.style.display)
-      ? 'block'
-      : 'none';
-  });
+  // // Nút "Ẩn/hiện danh sách" (#mc_view_list)
+  //   document.getElementById('mc_view_list').addEventListener('click', () => {
+  //   const wrapper = document.getElementById('mcTableWrapper');
+  //   wrapper.style.display = (wrapper.style.display === 'none' || !wrapper.style.display)
+  //     ? 'block'
+  //     : 'none';
+  // });
