d90d98cb7d277ce9fac0b4afed9f47141dc142cd
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-16 11:44:03 +0700

    Update mc_table.php

--- pages/mc/mc_table.php
+++ pages/mc/mc_table.php
@@ -115,19 +115,24 @@ $(function () {
       { extend: 'print', text: '🖨️ In bảng', title: 'Danh sách câu hỏi', className: 'toolbar-btn' }
     ],
     initComplete: function() {
-      // Chuyển nút DataTables vào toolbar-left
-      table.buttons().container().appendTo('#dtButtons');
+  // Append nút Excel/Print
+  table.buttons().container().appendTo('#dtButtons');
 
-      // Load chủ đề cho filter
-      const $filter = $('#filterTopic');
-      $.getJSON('../../includes/mc/mc_get_topics.php')
-        .done(topics => topics.forEach(t => $filter.append($('<option>', { value: t, text: t }))));
-      $filter.on('change', function() {
-        const val = this.value;
-        table.column(1).search(val ? '^' + $.fn.dataTable.util.escapeRegex(val) + '$' : '', true, false).draw();
-      });
-    },
-    drawCallback: function() { if (window.MathJax) MathJax.typesetPromise(); }
+  // Filter chủ đề
+  const $filter = $('#filterTopic');
+  $.getJSON('../../includes/mc/mc_get_topics.php')
+    .done(topics => topics.forEach(t => $filter.append($('<option>', { value: t, text: t }))));
+  $filter.on('change', function() {
+    const val = this.value;
+    table.column(1).search(val ? '^' + $.fn.dataTable.util.escapeRegex(val) + '$' : '', true, false).draw();
+  });
+
+  // Search box chung
+  $('#searchBox').on('keyup', function() {
+    table.search(this.value).draw();
+  });
+  }
+  drawCallback: function() { if (window.MathJax) MathJax.typesetPromise(); }
   });
 
   // Import Excel
