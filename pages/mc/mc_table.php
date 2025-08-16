68760462a1bd1fb931b5752a57410cea9264c255
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-16 12:08:40 +0700

    Update mc_table.php

--- pages/mc/mc_table.php
+++ pages/mc/mc_table.php
@@ -31,6 +31,11 @@ window.MathJax = {
   max-height: 80px;
   border-radius: 6px;
 }
+
+/* Ẩn search mặc định DataTables */
+.dataTables_filter {
+  display: none !important;
+}
 </style>
 </head>
 <body>
@@ -127,7 +132,7 @@ $(function () {
         table.column(1).search(val ? '^' + $.fn.dataTable.util.escapeRegex(val) + '$' : '', true, false).draw();
       });
 
-      // Search box toolbar đồng bộ với DataTables search
+      // Search toolbar đồng bộ
       $('#dtSearchBox').on('keyup', function() {
         table.search(this.value).draw();
       });
