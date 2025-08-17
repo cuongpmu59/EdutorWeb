aa85608a772bad5aa8b7da290fc9da544fc36482
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-17 09:20:39 +0700

    Update mc_table.php

--- pages/mc/mc_table.php
+++ pages/mc/mc_table.php
@@ -10,6 +10,7 @@
   };
   </script>
   <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js" async></script>
+  <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.4.0/css/fixedHeader.dataTables.min.css">
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
   <style>
     table img {
@@ -40,10 +41,21 @@
   <!-- JS thư viện -->
   <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
   <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+  <!-- JS FixedHeader -->
+  <script src="https://cdn.datatables.net/fixedheader/3.4.0/js/dataTables.fixedHeader.min.js"></script>
 
   <!-- File JS khởi tạo bảng -->
   <script src="../../js/mc/mc_fetch_data.js"></script>
   <script src="../../js/mc/mc_table_arrow_key.js"></script>
+
+  <script>
+    $(document).ready(function () {
+    $('#mcTable').DataTable({
+      paging: true,          // có thể bật phân trang
+      fixedHeader: true      // bật header cố định
+    });
+  });
+  </script>
 
 </body>
 </html>
