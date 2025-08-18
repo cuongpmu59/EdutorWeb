fe2cff01b87f5c07cdd75c9d6311896d387d5094
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-18 14:13:58 +0700

    Delete tf_preview.php

--- pages/tf/tf_preview.php
+++
@@ -1,8 +0,0 @@
-// =======================
-// File: pages/tf/tf_preview.php
-// =======================
-?>
-<div id="tf_preview_area"><em>⚡ Xem trước câu hỏi Đúng/Sai sẽ hiển thị ở đây...</em></div>
-<img id="tf_preview_image" style="display:none; max-height:150px; margin-top:10px; border:1px solid #ccc;">
-<?php
-
