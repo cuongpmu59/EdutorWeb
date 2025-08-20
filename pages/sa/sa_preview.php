1b7e3a15f6fa7f59c33f9da330b84852d2c4f0de
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-20 15:08:34 +0700

    Delete sa_preview.php

--- pages/sa/sa_preview.php
+++
@@ -1,7 +0,0 @@
-// =======================
-// File: pages/sa/sa_preview.php
-// =======================
-?>
-<div id="sa_preview_area"><em>⚡ Xem trước câu hỏi tự luận sẽ hiển thị ở đây...</em></div>
-<img id="sa_preview_image" style="display:none; max-height:150px; margin-top:10px; border:1px solid #ccc;">
-<?php
