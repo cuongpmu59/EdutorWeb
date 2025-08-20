f6f22e0f0093452752ce4e5787f24a40dc22ef9f
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-20 11:00:59 +0700

    Update tf_form_save.php

--- includes/tf/tf_form_save.php
+++ includes/tf/tf_form_save.php
@@ -15,47 +15,46 @@ try {
     $data = array_map('trim', $_POST);
     $tf_id = filter_var($data['tf_id'] ?? null, FILTER_VALIDATE_INT);
 
-    // Kiểm tra trường bắt buộc
-    $requiredFields = ['tf_topic', 'tf_question'];
-    for ($i = 1; $i <= 4; $i++) {
-        $requiredFields[] = 'statement'.$i;
-        $requiredFields[] = 'correct_answer'.$i;
-    }
-
+    $requiredFields = [
+        'tf_topic', 'tf_question',
+        'tf_statement1', 'tf_statement2', 'tf_statement3', 'tf_statement4',
+        'tf_correct_answer1', 'tf_correct_answer2', 'tf_correct_answer3', 'tf_correct_answer4'
+    ];
     foreach ($requiredFields as $field) {
         if (!isset($data[$field]) || $data[$field] === '') {
             exit(json_encode(['status' => 'error', 'message' => 'Vui lòng nhập đầy đủ thông tin.']));
         }
     }
 
+    // Cho phép để trống ảnh
     $tf_image_url = !empty($data['tf_image_url']) ? $data['tf_image_url'] : null;
 
     if ($tf_id) {
-        // Cập nhật
-        $sql = "UPDATE tf_questions SET 
-                    tf_topic=?, tf_question=?, 
-                    tf_statement1=?, tf_statement2=?, tf_statement3=?, tf_statement4=?,
-                    tf_correct_answer1=?, tf_correct_answer2=?, tf_correct_answer3=?, tf_correct_answer4=?,
+        $sql = "UPDATE tf_questions 
+                SET tf_topic=?, tf_question=?, 
+                    tf_statement1=?, tf_statement2=?, tf_statement3=?, tf_statement4=?, 
+                    tf_correct_answer1=?, tf_correct_answer2=?, tf_correct_answer3=?, tf_correct_answer4=?, 
                     tf_image_url=? 
                 WHERE tf_id=?";
         $types = "sssssssssssi";
         $params = [
             $data['tf_topic'], $data['tf_question'],
-            $data['statement1'], $data['statement2'], $data['statement3'], $data['statement4'],
-            $data['correct_answer1'], $data['correct_answer2'], $data['correct_answer3'], $data['correct_answer4'],
+            $data['tf_statement1'], $data['tf_statement2'], $data['tf_statement3'], $data['tf_statement4'],
+            $data['tf_correct_answer1'], $data['tf_correct_answer2'], $data['tf_correct_answer3'], $data['tf_correct_answer4'],
             $tf_image_url, $tf_id
         ];
     } else {
-        // Thêm mới
         $sql = "INSERT INTO tf_questions 
-                (tf_topic, tf_question, tf_statement1, tf_statement2, tf_statement3, tf_statement4,
-                 tf_correct_answer1, tf_correct_answer2, tf_correct_answer3, tf_correct_answer4, tf_image_url)
+                (tf_topic, tf_question, 
+                 tf_statement1, tf_statement2, tf_statement3, tf_statement4, 
+                 tf_correct_answer1, tf_correct_answer2, tf_correct_answer3, tf_correct_answer4, 
+                 tf_image_url) 
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         $types = "sssssssssss";
         $params = [
             $data['tf_topic'], $data['tf_question'],
-            $data['statement1'], $data['statement2'], $data['statement3'], $data['statement4'],
-            $data['correct_answer1'], $data['correct_answer2'], $data['correct_answer3'], $data['correct_answer4'],
+            $data['tf_statement1'], $data['tf_statement2'], $data['tf_statement3'], $data['tf_statement4'],
+            $data['tf_correct_answer1'], $data['tf_correct_answer2'], $data['tf_correct_answer3'], $data['tf_correct_answer4'],
             $tf_image_url
         ];
     }
@@ -68,7 +67,6 @@ try {
         'status' => 'success',
         'message' => $tf_id ? 'Cập nhật câu hỏi thành công.' : 'Thêm câu hỏi thành công.'
     ]);
-
 } catch (mysqli_sql_exception $e) {
     echo json_encode(['status' => 'error', 'message' => 'Lỗi CSDL: ' . $e->getMessage()]);
 } finally {
