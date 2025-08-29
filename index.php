8a5e0003e20f8c3031d8fc293455141f40e68a8a
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-30 05:18:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -309,7 +309,7 @@
 
         <label>Đánh giá (1-5 sao):</label>
         <div class="rating">
-          <input type="radio" id="star5" name="rating" value="5"><label for="star5">&#9733;</label>
+          <input type="radio" id="star5" name="rating" value="5"><label for="star5">&#9  733;</label>
           <input type="radio" id="star4" name="rating" value="4"><label for="star4">&#9733;</label>
           <input type="radio" id="star3" name="rating" value="3"><label for="star3">&#9733;</label>
           <input type="radio" id="star2" name="rating" value="2"><label for="star2">&#9733;</label>
