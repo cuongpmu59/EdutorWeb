4141bc604671bb40267e556e9a75232b61f540d3
cuongpmu59 <phungkhaccuong59@gmail.com>
2025-08-29 07:07:18 +0700

    Update sa_fetch_data.php

--- includes/sa/sa_fetch_data.php
+++ includes/sa/sa_fetch_data.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
-require_once __DIR__ . '/../db_connection.php';
+require_once __DIR__ . '/../../includes/env/db_connection.php';
 
 // Lấy các tham số từ DataTables
 $draw   = intval($_POST['draw'] ?? 0);
